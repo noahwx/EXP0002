78311698857dc9f7c69d11cb6e5fc3a07ce7d13f
Noah Fontenette <noahfontenette@outlook.com>
2022-12-09 17:41:55 -0600

    0.0.006

--- my-app/src/pages/Search.js
+++ my-app/src/pages/Search.js
@@ -1,11 +1,47 @@
 import '../index.css';
+import {View, TextInput, StyleSheet} from 'react-native'
+import React, {useState} from 'react';
 
 const Search = () => {
+
+    const [searchInput, setSearchInput] = useState("");
+
+    const stories = [
+        {name: 'Keep a Eye on this One Stock', author: 'author', date: '12/07/22'},
+    ]
+    
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    };
+    
+    if (searchInput.length > 0) {
+        stories.filter((story) => {
+            return story.name.match(searchInput);
+        });
+    }
+
     return (
         <>
             <div className='container'>
                 <div className='container-info'>
                     <h1 className='s-title'>Search</h1>
+                </div>
+                <div className="search-container">
+                    <View>
+                        <TextInput placeholder="Search..." style={styles.input} onChange={handleChange} value={searchInput}/>
+                        <table>
+                            <tr>
+                                <th>Story</th>
+                            </tr>
+
+                            {stories.map((story, stories) => {
+                                <tr>
+                                    <td>{stories.name}</td>
+                                </tr>
+                            })}
+                        </table>
+                    </View>
                 </div>
                 <div className='S-BG'></div>
             </div>
@@ -14,3 +50,16 @@ const Search = () => {
 }
  
 export default Search;
+
+const styles = StyleSheet.create({
+    input: {
+      borderColor: "black",
+      width: "80vw",
+      borderWidth: 2,
+      borderRadius: 20,
+      padding: 1,
+      textAlign: 'center',
+      fontFamily: 'bungee,sans-serif',
+      fontSize: 15,
+    },
+});
